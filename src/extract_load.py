0c94dc4869dc2092af39af8bb9be7850a03f47cc
Oiluj <juliogabriel.pe@gmail.com>
2024-06-16 22:55:31 -0300

    third commit

---
+++ src/extract_load.py
@@ -0,0 +1,44 @@
+# imports
+import yfinance as yf
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+# imports environments variables
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+# pegar a cotacao dos ativos
+commodities = ['CL=F', 'GC=F', 'SI=F']
+
+def search_commodities_data(ticker, period='5d', interval='1d'):
+    ticker = yf.Ticker(ticker)
+    data = ticker.history(period=period, interval=interval)[['Close']]
+    data['ticker'] = ticker
+    return data
+
+def search_commodities_data_all(commodities):
+    all_data = []
+    for ticker in commodities:
+        data = search_commodities_data(ticker)
+        all_data.append(data)
+    return pd.concat(all_data)
+
+def save_in_postgres(df, schema='public'):
+    df.to_sql('commodidites', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
+if __name__ == "__main__":
+    concatenated_data = search_commodities_data_all(commodities)
+    save_in_postgres(concatenated_data, schema='public')
